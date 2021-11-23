d062755ae0d2ab5156b78eaf7c510f618b8efed0
Harrisonp365 <harrisonpowell365@gmail.com>
2021-11-24 09:53:19 +1100

    list of users

---
+++ index.js
@@ -0,0 +1,1 @@
+const CODEWARS_URL = "https://www.codewars.com/api/v1/users";
---
+++ users.js
@@ -0,0 +1,17 @@
+const CONSULTANTS_CODEWARS = {
+  cheynemh: "Cheyne Murdoch-Hall",
+  smllygrl: "Natalie Lovibond Hodgson",
+  "Jinn-T": "Jinn Tsang",
+  martynakr: "Martyna Krol",
+  Harrisonp365: "Harrison Powell",
+  sebzo65: "Sebastian Penedo",
+  SGray1255: "Stephen Gray",
+  blakelists: "Blake Stickland",
+  helexander: "Jeremy Chee",
+  sandramyrda: "Sandra Myrda",
+  redprakash: "Prakash Poudel",
+  Wondermelon98: "Tim Ng",
+  YujinOk: "Yujin",
+  HowLargeTheTeeth: "Emily Graham",
+  b0rgsta: "Andrew Borg",
+};
